2011b97e71a555f0badf6c0fa1ca98efe8cb8829
ESA <esaul-00@mail.ru>
2021-04-26 15:12:41 +0300

    100% create desktop version

---
+++ src/js/modul.js
@@ -0,0 +1,5 @@
+function output_console (){
+  console.log("visuality menu");
+}
+
+export default output_console;
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,30 @@
-const menu = document.getElementsByClassName('menu')[0],
-      nav = document.getElementsByTagName('nav')[0];
+import output_console from './modul.js'
+import conslog from './modul.js'
 
+const menu = document.getElementsByClassName('menu')[0],
+      menu_span = menu.children[0],
+      nav = document.getElementsByTagName('nav')[0],
+      header_row = document.getElementsByClassName('row');
+
+
+/*
+определить координаты элемента с помощью функций offsetLeft, offsetHeight, offsetWidth, offsetTop.
+Чтобы меню выдвигалось ровно по сетке bootstrap нужно к координате offsetLeft элемента nav прибавлять offsetLeft элемента nav(только положительное число) плюс координату offsetLeft элемента header_nav
+*/
 menu.onclick = function () {
-  alert('hello');
+  // переменная visible_nav_position хранит вычисленную позицию на которую переместится элемент меню при вызове меню
+  if(menu.classList == 'menu menu_active'){
+    let visible_nav_position = 0 - (nav.offsetLeft + nav.offsetWidth);
+    nav.style.transform = 'translateX(' + visible_nav_position + 'px)';
+    nav.style.opacity = "0";
+  }
+  else{
+    let visible_nav_position = Math.abs(nav.offsetLeft) + header_row[0].offsetLeft;
+    nav.style.transform = 'translateX(' + visible_nav_position + 'px)';
+    nav.style.opacity = "1";
+  }
+  menu.classList.toggle('menu_active');
+  menu_span.classList.toggle('menu_active_span');
+  output_console();
+  conslog();
 }
